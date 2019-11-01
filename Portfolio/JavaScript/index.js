df2de299dabee406c4b1e4019a1f2b9602e81d9c
Philip <phchen97@gmail.com>
2019-10-31 23:32:57 -0700

    more progress and added JS folder

---
+++ Portfolio/JavaScript/index.js
@@ -0,0 +1,20 @@
+$(function(){
+	$(window).resize(function() {
+		var $t = $(".noDivider");
+	  	if ($(window).width() < 768){
+	        $t.addClass("noDivider");
+	    }
+	    else{
+	    	$t.removeClass("noDivider");
+	    }
+	});
+});
+
+function scrollToContact() {
+	var currentUrl = document.URL;
+	if(currentUrl && currentUrl.includes("portfolio.html")){
+	  	window.location = "index.html";
+	}
+	var contact = document.getElementById("contact");
+	contact.scrollIntoView();
+}
